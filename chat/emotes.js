b2ae0e1eeb404117e62bf7c09923327bd8ed5d3b
Optimusys <159470517+epltv1@users.noreply.github.com>
2026-05-08 22:38:46 +0300

    Create emotes.js with predefined emote list
    Added a collection of emotes with names and URLs.

---
+++ chat/emotes.js
@@ -0,0 +1,78 @@
+export const emotes = [
+  {
+    name: "GIGACHAD",
+    url: "https://cdn.7tv.app/emote/01F6MZGCNG000255K4X1K7NTHR/4x.webp"
+  },
+  {
+    name: "donowall",
+    url: "https://cdn.7tv.app/emote/01F6BN89H80006VBW12DRB1DJ0/4x.webp"
+  },
+  {
+    name: "catJAM",
+    url: "https://cdn.7tv.app/emote/01F6MQ33FG000FFJ97ZB8MWV52/4x.webp"
+  },
+  {
+    name: "modCheck",
+    url: "https://cdn.7tv.app/emote/01F6FTE8B80008E39HFFQJ7MWS/4x.webp"
+  },
+  {
+    name: "Hmm",
+    url: "https://cdn.7tv.app/emote/01F6MA6Y100002B6P5MWZ5D916/4x.webp"
+  },
+  {
+    name: "peepoLeave",
+    url: "https://cdn.7tv.app/emote/01G4ZGET1R0003SYTMXJ2SQCGP/4x.webp"
+  },
+  {
+    name: "PETTHEMODS",
+    url: "https://cdn.7tv.app/emote/01F6NF001G00044EM07K5E6WYK/4x.webp"
+  },
+  {
+    name: "SadgeCry",
+    url: "https://cdn.7tv.app/emote/01F6MMZW3R00012ZP6HJJ38G2E/4x.webp"
+  },
+  {
+    name: "NOWAYING",
+    url: "https://cdn.7tv.app/emote/01GQ3WPTDR000300KQF1J7PFF8/4x.webp"
+  },
+  {
+    name: "WHAT",
+    url: "https://cdn.7tv.app/emote/01G4ZTECKR0002P97QQ94BDSP4/4x.webp"
+  },
+  {
+    name: "HUH",
+    url: "https://cdn.7tv.app/emote/01FFMS6Q4G0009CAK0J14692AY/4x.webp"
+  },
+  {
+    name: "LETHIMCOOK",
+    url: "https://cdn.7tv.app/emote/01GN5QQ1X8000C0QTR0C8JV0GR/4x.webp"
+  },
+  {
+    name: "PeepoFinger",
+    url: "https://cdn.7tv.app/emote/01F779KZC8000D25DGAC8PTX5Q/4x.webp"
+  },
+  {
+    name: "HACKERMANS",
+    url: "https://cdn.7tv.app/emote/01F6WP22CR0004YCK11WAVZHEW/4x.webp"
+  },
+  {
+    name: "WhoAsked",
+    url: "https://cdn.7tv.app/emote/01FNSSG3T8000EJT2EVEY3FBSY/4x.webp"
+  },
+  {
+    name: "TriSad",
+    url: "https://cdn.7tv.app/emote/01EZPGQES8000C438200A44F0M/4x.webp"
+  },
+  {
+    name: "Cinema",
+    url: "https://cdn.7tv.app/emote/01GBFDVP18000CRDCG0DV7KEMY/4x.webp"
+  },
+  {
+    name: "VeryPog",
+    url: "https://cdn.7tv.app/emote/01EZPGPAN0000C438200A44F0K/4x.webp"
+  },
+  {
+    name: "Ghost",
+    url: "https://cdn.7tv.app/emote/01GHRZNKG8000CMMVPKY84QCSW/4x.webp"
+  }
+];
